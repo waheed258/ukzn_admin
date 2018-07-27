44f6bd13cbc2a6956e3bd8655ae0efb867128f09
Anitha <anitha.kodati@dinoosys.com>
2018-07-27 13:04:00 +0530

    I Have changed UI of these forms as per new template
     many css and Js files had been added in viewFileDeatils form in previous , I have commented those files which are not mandatory. If those will necessary to add in future you can add as per your requirement

---
+++ WebUKZN/SalesAdmin/ViewDocument.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SalesAdmin_ViewDocument : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["FileName"] != null)
+        {
+            string strPath = Server.MapPath("FileDocuments/" + Request.QueryString["FileNo"] + "/" + Request.QueryString["FileName"]); ;
+            System.IO.FileInfo file = new System.IO.FileInfo(strPath);
+
+            if (file.Exists == true)
+            {
+                frmDoc.Attributes.Add("src", strPath);
+            }
+        }
+
+
+
+    }
+}
